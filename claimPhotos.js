0afb75b475e30b6230dbe293cb99877288b98ba5
Julian Verschuren <julianverschuren@hotmail.com>
2016-05-25 14:22:08 +0200

    commit 2: key event
    Ook met de enter toets is het nu mogelijk

--- claimPhotos.js
+++ claimPhotos.js
@@ -6,8 +6,8 @@ $(document).ready(function () {
 
 
 			searching = $('#searchTerm').val();
-
-			alert(searching);
+			getPhotos();
+			//alert(searching);
 
 		}
 
@@ -21,14 +21,31 @@ $(document).ready(function () {
 
 
 
-$('#searchTerm').keydown(function(){
+$('#searchTerm').keydown(function(e){
 
+if (e.keyCode == 13){
 
+searching = $('#searchTerm').val();
+getPhotos();
+//alert(searching);
+
+}
 
 
 
 })
+//wat hierboven gebeurd is bijna het zelfde als wat er in de eerste functie gebeurt maar dan ietsje anders
+// hier maak ik een function die start op het moment dat je op je enter toets drukt. als je dus ''banaan'' invult op de plaats van de zoekterm in de zoekbalk en je drukt op enter dan logt ie ''banaan''
+//de event is een keydown event in plaats van een click event. argument dat je mee geeft is e van event, als event.keyCode gelijk is aan nummer 13 (dat is de codenaam van enter) dan krijgt de variabele searching die al eerder aangemaakt is de waarde van de searchTerm. dan start de functie en alert je searching weer.
+//ik zou eventueel searchTerm kunnen vervangen met ''this'' want het is namelijk hetzelfde als het gene dat de gebeurtenis veroorzaakt. voor het gemak en duidelijkheid heb ik gewoon searchTem laten staan
 
 
+function getPhotos(){
+
+alert(searching);
+
+}
 
 })
+
+//inplaats van dat ik per functie alert searching doe.  heb ik een functie aangemaakt genaamd getPhotos. die functie alert searching en die functie kan ik nu overal aanroepen. dit is een stuk handiger dan elke keer overnieuw hem moeten aanroepen .
